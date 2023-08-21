5f7a7d5e77c7eb00508873df5af71d987f1cbfa3
arman <ruhit.arman@dohatec.com.bd>
2023-08-21 10:32:18 +0600

    send message through other apps

--- MultpleActivities/app/src/main/java/com/example/multpleactivities/CreateMessageActivity.java
+++ MultpleActivities/app/src/main/java/com/example/multpleactivities/CreateMessageActivity.java
@@ -2,26 +2,36 @@ package com.example.multpleactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
     private Button sendMessageButton;
+    private EditText messageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_message);
         sendMessageButton = findViewById(R.id.send);
+        messageEditText = findViewById(R.id.messageEditText);
 
         //Add onClickListern on sendMessageButton
         sendMessageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CreateMessageActivity.this, RecieveMessageActivity.class);
+                String message = messageEditText.getText().toString();
+//                Intent intent = new Intent(CreateMessageActivity.this, RecieveMessageActivity.class);
+//                intent.putExtra("message", message);
+
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, message);
                 startActivity(intent);
             }
         });
--- MultpleActivities/app/src/main/java/com/example/multpleactivities/RecieveMessageActivity.java
+++ MultpleActivities/app/src/main/java/com/example/multpleactivities/RecieveMessageActivity.java
@@ -2,13 +2,20 @@ package com.example.multpleactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RecieveMessageActivity extends AppCompatActivity {
 
+    private TextView messageText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recieve_message);
+        messageText = findViewById(R.id.messageText);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        messageText.setText(message);
     }
 }
