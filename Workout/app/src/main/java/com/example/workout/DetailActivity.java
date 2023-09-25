cbd0966db3d06dab51cb258555c99c31c9950773
ArmanRuhit <armanruhit09@gmail.com>
2023-09-25 23:54:05 +0600

    Workout Done

---
+++ Workout/app/src/main/java/com/example/workout/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.workout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    public static final String WORKOUT_ID="id";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        WorkoutDetailFragment workoutDetailFragment = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        int workoutId = (int) getIntent().getExtras().get(WORKOUT_ID);
+        workoutDetailFragment.setWorkoutId(workoutId);
+    }
+}
--- Workout/app/src/main/java/com/example/workout/MainActivity.java
+++ Workout/app/src/main/java/com/example/workout/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.workout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity implements WorkoutListFragment.WorkoutListListener {
@@ -18,12 +20,19 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
 
     @Override
     public void itemClicked(long id) {
-        WorkoutDetailFragment workoutDetailFragment = new WorkoutDetailFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        workoutDetailFragment.setWorkoutId(id);
-        fragmentTransaction.replace(R.id.detail_frag_container, workoutDetailFragment);
-        fragmentTransaction.addToBackStack(null);
-        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        fragmentTransaction.commit();
+        View fragmentContainer = findViewById(R.id.detail_frag_container);
+        if(fragmentContainer != null) {
+            WorkoutDetailFragment workoutDetailFragment = new WorkoutDetailFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            workoutDetailFragment.setWorkoutId(id);
+            fragmentTransaction.replace(R.id.detail_frag_container, workoutDetailFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        } else{
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
     }
 }
